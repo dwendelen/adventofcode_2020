687e5af873f15cfa8768ca2e5b29d190a3dc2d4c
Daan Wendelen <daanwendelen@gmail.com>
2020-12-18 16:49:29 +0000

    Day17

---
+++ day17/day17_b/main.cpp
@@ -0,0 +1,116 @@
+#include <map>
+#include <iostream>
+#include "../../util.hpp"
+
+class Coordinate {
+public:
+    Coordinate(int64_t x, int64_t y, int64_t z, int64_t w) : x(x), y(y), z(z), w(w) {}
+
+    int64_t x;
+    int64_t y;
+    int64_t z;
+    int64_t w;
+
+    bool operator<(const Coordinate &other) const {
+        if (x < other.x) {
+            return true;
+        } else if (x > other.x) {
+            return false;
+        }
+
+        if (y < other.y) {
+            return true;
+        } else if (y > other.y) {
+            return false;
+        }
+
+        if (z < other.z) {
+            return true;
+        } else if (z > other.z) {
+            return false;
+        }
+
+        if (w < other.w) {
+            return true;
+        } else if (w > other.w) {
+            return false;
+        }
+
+        return false;
+    }
+};
+
+int main() {
+    std::vector<std::string> lines = readStrings("day17/input.txt");
+
+    std::map<Coordinate, bool> map1;
+    std::map<Coordinate, bool> map2;
+
+    std::map<Coordinate, bool> &current = map1;
+    std::map<Coordinate, bool> &next = map2;
+
+    for (int y = 0; y < lines.size(); ++y) {
+        auto line = lines[y];
+        for (int x = 0; x < line.size(); ++x) {
+            if (line[x] == '#') {
+                current[Coordinate(x, y, 0, 0)] = true;
+            }
+        }
+    }
+
+    for (int i = 0; i < 6; i++) {
+        for (auto &entry: current) {
+            const Coordinate &element = entry.first;
+            for (int64_t xx = element.x - 1; xx <= element.x + 1; ++xx) {
+                for (int64_t yy = element.y - 1; yy <= element.y + 1; ++yy) {
+                    for (int64_t zz = element.z - 1; zz <= element.z + 1; ++zz) {
+                        for (int64_t ww = element.w - 1; ww <= element.w + 1; ++ww) {
+                            const Coordinate &neighbour = Coordinate(xx, yy, zz, ww);
+                            if (next.count(neighbour) == 1) {
+                                continue;
+                            }
+
+                            int count = 0;
+                            for (int64_t xxx = xx - 1; xxx <= xx + 1; ++xxx) {
+                                for (int64_t yyy = yy - 1; yyy <= yy + 1; ++yyy) {
+                                    for (int64_t zzz = zz - 1; zzz <= zz + 1; ++zzz) {
+                                        for (int64_t www = ww - 1; www <= ww + 1; ++www) {
+                                            if (xxx == xx && yyy == yy && zzz == zz && www == ww) {
+                                                continue;
+                                            }
+                                            count += current.count(Coordinate(xxx, yyy, zzz, www));
+                                        }
+                                    }
+                                }
+                            }
+                            if (current.count(neighbour) == 0) {
+                                if (count == 3) {
+                                    next[neighbour] = true;
+                                } else {
+                                    next[neighbour] = false;
+                                }
+                            } else {
+                                if (count == 2 || count == 3) {
+                                    next[neighbour] = true;
+                                } else {
+                                    next[neighbour] = false;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        for (auto it = next.begin(); it != next.end();) {
+            if (!it->second) {
+                it = next.erase(it);
+            } else {
+                ++it;
+            }
+        }
+        std::swap(current, next);
+        next.clear();
+    }
+
+    std::cout << current.size() << std::endl;
+}
